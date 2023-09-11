394d1f02f6c24255be5474699a9641e948c005aa
tfchowdhury <127281818+tfchowdhury@users.noreply.github.com>
2023-09-10 22:11:27 -0500

    instagram_data.sql

--- ig_clone_data (1).sql
+++ ig_clone_data (1).sql
@@ -1,6 +1,5 @@
-DROP DATABASE IF EXISTS ig_clone;
-CREATE DATABASE ig_clone;
-USE ig_clone; 
+CREATE DATABASE instagram_data;
+USE instagram_data; 
 
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
